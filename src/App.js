6d900c4716fffa50eaa558b7e2a0d788764ea1ef
beanbeeean <beanbeeean@naver.com>
2023-06-16 15:33:39 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Search from "./pages/Search";
 import Playlists from "./playlist_components/Playlists";
 import Sign from "./pages/Sign";
 import { useRef, useState } from "react";
+import Charts from "./charts_components/Charts";
+import PlaylistItem from "./playlist_components/PlaylistItem";
+import CreatePlaylist from "./playlist_components/CreatePlaylist";
+import SignIn from "./sign_components/SignIn";
+import SignUp from "./sign_components/SignUp";
 
 function App() {
   let login = useRef(window.localStorage.getItem("session"));
@@ -19,7 +24,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/playlist" element={<Playlists />} />
-        <Route path="/sign" element={<Sign login={login} />} />
+        <Route path="/charts" element={<Charts />} />
+        <Route path="/signin" element={<SignIn login={login} />} />
+        <Route path="/signup" element={<SignUp login={login} />} />
+        <Route path="/playlistitem" element={<PlaylistItem />} />
+        <Route path="/playlist/create_playlist" element={<CreatePlaylist />} />
       </Routes>
       <Footer />
     </div>
