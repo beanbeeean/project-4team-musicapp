96f525794f20edf8e7a780cb02701d34b0ff19b7
gilgan9852 <gilgan9852@gmail.com>
2023-06-21 19:12:32 +0900

    플레이리스트 복제

--- src/playlist_components/AllPlaylist.jsx
+++ src/playlist_components/AllPlaylist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./css/all_playlist.module.css";
 import { Col, Container, Row } from "react-bootstrap";
@@ -9,8 +9,35 @@ import { Link, useLocation } from "react-router-dom";
 
 const AllPlaylist = () => {
   let playlist = JSON.parse(window.localStorage.getItem("playlist"));
-
+  let m_id = window.localStorage.getItem("session");
   let curPlaylist;
+
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [date, setDate] = useState("");
+  const [create_date, setCreate_date] = useState();
+  
+  const getNowDate = () => {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month =
+      today.getMonth() + 1 < 10
+        ? "0" + (today.getMonth() + 1)
+        : today.getMonth() + 1;
+    let date =
+      today.getDate() < 10 ? "0" + today.getDate() < 10 : today.getDate();
+
+    setYear(year);
+    setMonth(month);
+    setDate(date);
+    setCreate_date(`${year}.${month}.${date}`);
+  };
+
+  useEffect(() => {
+    getNowDate();
+  }, []);
+
+
   const curPlaylists = (idx) => {
     curPlaylist = !playlist[idx]
       ? null
@@ -19,6 +46,33 @@ const AllPlaylist = () => {
     console.log(playlist);
     console.log(curPlaylist);
   };
+
+  const copyPlaylist = (e, playlist_title, copy_key) => {
+    let playlist = [
+      {
+        playlist_title: playlist_title,
+        about_playlist: "",
+        create_date: create_date,
+      },
+    ];
+    let user = JSON.parse(window.localStorage.getItem(m_id));
+    user = [...user, ...playlist];
+    window.localStorage.setItem(m_id, JSON.stringify(user));
+
+    let allplaylist=JSON.parse(window.localStorage.getItem("playlist"));
+    let allplay = { playlist_title: playlist_title, playlist_key: m_id+playlist_title, player: m_id};
+    allplaylist.push(allplay);
+    window.localStorage.setItem("playlist", JSON.stringify(allplaylist));
+
+    let copylist = JSON.parse(window.localStorage.getItem(copy_key))[0].tracks.items;
+    let copy = [];
+    console.log(copylist[0].track);
+    copylist.map((item, idx) =>
+      copy.push({num:idx, item:copylist[idx].track})
+    )
+    if(copylist!==null)
+      window.localStorage.setItem(m_id+playlist_title, JSON.stringify(copy));
+  } 
 
   // console.log(curPlaylist);
 
@@ -54,14 +108,14 @@ const AllPlaylist = () => {
                           }
                         />
                       </Link>
-
-                      <div className={styles.album_play}>
-                        <FontAwesomeIcon
-                          className={styles.play_icon}
-                          icon={faCopy}
-                        />
-                      </div>
-
+                      <a href="#none" onClick={(e) => copyPlaylist(e, playlist[idx].playlist_title, playlist[idx].playlist_key)}>
+                        <div className={styles.album_play}>
+                          <FontAwesomeIcon
+                            className={styles.play_icon}
+                            icon={faCopy}
+                          />
+                        </div>
+                      </a>
                       <div>
                         <Link
                           to="/playlistitem"
@@ -123,13 +177,14 @@ const AllPlaylist = () => {
                         />
                       </Link>
 
+                      <a href="#none" onClick={(e) => copyPlaylist(e, playlist[idx].playlist_title, playlist[idx].playlist_key)}>
                       <div className={styles.album_play}>
                         <FontAwesomeIcon
                           className={styles.play_icon}
                           icon={faCopy}
                         />
                       </div>
-
+                      </a>
                       <div>
                         <Link
                           to="/playlistitem"
